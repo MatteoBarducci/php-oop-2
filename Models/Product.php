95309e61b1d1a8ef0e6004729b5ccced1197da6a
MatteoBarducci <mbardu1301@gmail.com>
2024-05-16 22:07:07 +0200

    feat add kennel class

--- Models/Product.php
+++ Models/Product.php
@@ -6,10 +6,10 @@ class Product {
     public $category;
     public $description;
 
-    public function __construct($_name, $_price, $_category){
+    public function __construct($_name, $_category, $_price){
         $this->name = $_name;
-        $this->price = $_price;
         $this->category = $_category;
+        $this->price = $_price;
     }
 
 }
--- index.php
+++ index.php
@@ -10,11 +10,19 @@ articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia). -->
 
 require_once __DIR__ . '/Models/Product.php';
 require_once __DIR__ . '/Models/Food.php';
+require_once __DIR__ . '/Models/Game.php';
+require_once __DIR__ . '/Models/Kennel.php';
 
 
-$product = new Product('Collare', 'Cane', 9.99);
+
+
+$product = new Product('Collare antiparassitario', 'Cane', 9.99, 'Collare che repelle le zecche e pulci fino a 8 mesi');
 var_dump($product);
 $croccantini = new Food ('Croccantini', 'Gatto', 14.99, 500, 350, 'carne');
 var_dump($croccantini);
+$osso_di_gomma = new Game ('Osso di gomma', 'Cane', 7.99);
+var_dump($osso_di_gomma);
+$cuccia_da_esterno = new Kennel ('Cuccia da esterno', 'Cane', 49.99);
+var_dump($cuccia_da_esterno);
 
 ?>
